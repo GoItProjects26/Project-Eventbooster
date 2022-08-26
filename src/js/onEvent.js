199bbec947d2db927f5b2a0d4b15bcac0a528084
Maksymenko Maksym <maksymenko1992@ukr.net>
2022-08-26 23:20:42 +0300

    clean code

--- src/js/onEvent.js
+++ src/js/onEvent.js
@@ -1,7 +1,5 @@
 import { refs } from './refs';
 import { renderMarckup } from './renderHtml'
 
-console.log(refs.fetchBtn);
-
 refs.fetchBtn.addEventListener('click', renderMarckup);
 // window.addEventListener("load", loadRandomEvent);
