18d4e3b45c763552ded7bae5c3d47c8e9b700052
skazzp <skazzp@gmail.com>
2022-08-26 21:05:38 +0300

    del cons log

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,7 +1,6 @@
 let modalTest = document.querySelector('.modal__open');
 let backdrop = document.querySelector('.backdrop');
 let modalCloseBtn = document.querySelector('.modal__close-btn');
-console.log(modalTest);
 modalTest.addEventListener('click', openModal);
 modalCloseBtn.addEventListener('click', closeModal);
 function openModal() {
