570732c3cb24e00008e060fab9c9005ed84742d5
GoItProjects26 <112173159+GoItProjects26@users.noreply.github.com>
2022-08-26 17:32:52 +0000

    Deploying to gh-pages from @ GoItProjects26/Project-Eventbooster@004d5b08b46725e4c1e245b26697795b55c5cff8 🚀

---
+++ index.144db641.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".modal__open"),c=document.querySelector(".backdrop"),n=document.querySelector(".modal__close-btn");console.log(e),e.addEventListener("click",(function(){c.classList.remove("is-hidden")})),n.addEventListener("click",(function(){c.classList.add("is-hidden")}))}();
+//# sourceMappingURL=index.144db641.js.map
