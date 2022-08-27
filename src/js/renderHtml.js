3fd4f83d6c752e8674f4972a37384582b0304480
Maksymenko Maksym <maksymenko1992@ukr.net>
2022-08-27 10:45:15 +0300

    icons

--- src/js/renderHtml.js
+++ src/js/renderHtml.js
@@ -1,8 +1,7 @@
 import { refs } from './refs';
 import { EventApi } from './api'
-
+//master function
 const event = new EventApi;
-
 
 export async function renderMarckup() {
     try {
@@ -15,7 +14,7 @@ export async function renderMarckup() {
     }
 }
 
-
+//slave function
 function marckup(eventsArray) {
     const marckupArray = eventsArray.map(event => {
         return templateItems(event)
@@ -24,7 +23,7 @@ function marckup(eventsArray) {
     refs.eventList.innerHTML = marckupArray.join('')
 }
 
-
+// template for cards
 function templateItems(event) {
     return `
    <li class="list_item" data-id ="${event.id}">
@@ -38,17 +37,21 @@ function templateItems(event) {
         </div>
         <h3 class="event_title">${event.name}</h3>
         <p class="event_date">${event.localDate}</p>
-        <p class="event_location">${event.concertHall}</p>
+        <p class="event_location location">
+        <svg  class="location_icon" width = 10 height = 7>
+        <use href="./images/icons.svg"></use>
+        </svg>
+        <span class="location_name">${event.concertHall}</span></p>
       </li>
 `
 }
-
+//sort out array of objects from server [{1,2,3},{1,2,3}.....] => [{1}]
 function shortDataFromServer(eventsArrayFull) {
     const arrayOfDesiredObjcts = eventsArrayFull.map(value => desiredObjectForPage(value))
     return arrayOfDesiredObjcts;
 }
 
-
+//generate object with less key:value from incoming object  to make markup for home page
 function desiredObjectForPage(value) {
     return {
         id: value.id,
@@ -58,7 +61,7 @@ function desiredObjectForPage(value) {
         concertHall: value._embedded.venues?.[0].name,
     }
 }
-
+//will be logic for 1st time visit our site
 function loadRandomEvent() {
 
 }
