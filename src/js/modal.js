4b20ba83571351933f1088694d1dc537b8c7e31e
Maksymenko Maksym <maksymenko1992@ukr.net>
2022-08-27 13:38:42 +0300

    Update modal.js

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,9 +1,11 @@
+import { refs } from './refs';
+
 let modalTest = document.querySelector('.modal__open');
 let backdrop = document.querySelector('.backdrop');
 let modalCloseBtn = document.querySelector('.modal__close-btn');
 console.log(modalTest);
-modalTest.addEventListener('click', openModal);
-modalCloseBtn.addEventListener('click', closeModal);
+refs.modalTest.addEventListener('click', openModal);
+refs.modalCloseBtn.addEventListener('click', closeModal);
 function openModal() {
   backdrop.classList.remove('is-hidden');
 }
