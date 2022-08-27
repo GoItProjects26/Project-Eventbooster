fed6ca7c307f95884e1a24821579f4c2e20a6da2
seacat271 <seacat271083@gmail.com>
2022-08-27 19:10:59 +0300

    add js

--- src/js/mini-modal.js
+++ src/js/mini-modal.js
@@ -1,5 +1,21 @@
 import {refs} from "./refs";
 import userBasket from "./basket";
+import {Basket} from "./basket";
+import userEventApi from "./api"
+import {closeModal} from "./modal"
+const userBasket = new Basket; //должно создаваться при загрузке Фетча
+refs.modalBuyBtn.addEventListener("click", onClickModalBuyBtn);
+function onClickModalBuyBtn (event) {
+    refs.miniModal.classList.toggle("hidden");
+ 
+    const basketId = event.target.closest("[data-id]").dataset.id
+    userBasket.addEvent(basketId)
+    userBasket.increaseStandardQuantity ()
+
+
+
+}
+
 
 refs.miniModalBtnClose.addEventListener("click", onClickMiniModalBtnClose);
 
@@ -11,15 +27,18 @@ refs.miniModalBtnCart.addEventListener("click", onClickMiniModalBtnOpenBasket);
 
 function onClickMiniModalBtnOpenBasket (event) {
     refs.miniModal.classList.toggle("hidden")
-    //закрывает модалку//
+    closeModal();
     refs.basketModal.classList.toggle("hidden")
     refs.basketQuantity.textContent = userBasket.totalQuantity;
-    // renderBasketMarkup(data)/// Данные с именем события
+    refs.basketNum.textContent = userBasket.totalQuantity;
+    if (userBasket.totalQuantity !== 0 && refs.basketNum.classList.contains("hidden")) refs.basketContainer.classList.remove("hidden")
+    localStorage.setItem("userBasket", JSON.stringify(userBasket));
+    renderBasketMarkup(userBasket.contentShoppingCart)/// Данные с именем события
 }
 
 function renderBasketMarkup (data) {
     let markup = "";
-    data.forEach(({name}) => {
+    data.forEach(name => {
 markup += `<li><div><div class="modal-basket__name">${name}</div></div></li>`
     })
     refs.basketMarkupContainer.insertAdjacentHTML("beforeend", markup);
