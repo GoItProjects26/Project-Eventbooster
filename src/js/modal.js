8afff684ab3ec255d5c47b6080f3ebacdbab746b
seacat271 <seacat271083@gmail.com>
2022-08-28 22:08:50 +0300

    Update modal.js

--- src/js/modal.js
+++ src/js/modal.js
@@ -4,7 +4,7 @@ let modalTest = document.querySelector('.modal__open');
 let backdrop = document.querySelector('.backdrop');
 let modalCloseBtn = document.querySelector('.modal__close-btn');
 
-// modalTest.addEventListener('click', openModal);
+modalTest.addEventListener('click', openModal);
 modalCloseBtn.addEventListener('click', closeModal);
 
 export function openModal() {
