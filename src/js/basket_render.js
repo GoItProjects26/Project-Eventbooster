941e25fa55604563b9256efbb0b45ed2e66caea7
seacat271 <seacat271083@gmail.com>
2022-08-29 21:42:47 +0300

    add markup

--- src/js/basket_render.js
+++ src/js/basket_render.js
@@ -6,8 +6,12 @@ function renderBasketMarkup(data) {
    
     refs.basketMarkupContainer.innerHTML = '';
     let markup = '';
-    data.forEach(({name}) => {
-      markup += `<li><div><div class="modal-basket__name">${name}</div></div></li>`;
+    let url = data.images
+    // .filter((item) => {
+    //   item.ratio === "3_2"    })
+      console.log(url)
+    data.forEach(({name, url}) => {
+      markup += `<li class="event__item"><div class="event__container"><img href="" class="event__img" width="30" height="30"><p class="event__text">${name}</p></div></li>`;
     });
     refs.basketMarkupContainer.insertAdjacentHTML('beforeend', markup);
   }
