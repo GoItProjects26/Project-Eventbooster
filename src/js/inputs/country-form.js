cc3741f17e37e38e0e9c1ed46d2d83d115ff9d71
BogdanGlazkov <bglazkov@i.ua>
2022-08-29 16:56:42 +0300

    Add search by country

--- src/js/inputs/country-form.js
+++ src/js/inputs/country-form.js
@@ -1,11 +1,15 @@
 import { refs } from '../refs';
 import { EventApi } from '../api';
 import { renderMarckup } from '../renderHtml';
+import { countryCodes } from '../country/countryList';
 
-refs.countryForm.addEventListener('change', onCountrySearch);
+refs.countryForm.addEventListener('submit', onCountrySearch);
 
 function onCountrySearch(event) {
-  const country = refs.countryForm.elements.countrySelect.value;
+  event.preventDefault();
+  const countryName = refs.countryForm.elements.countryQuery.value;
+  const countryObj = countryCodes.find(country => country.name === countryName);
+  const country = countryObj.code;
   EventApi.setCountry(country);
   renderMarckup();
 }
