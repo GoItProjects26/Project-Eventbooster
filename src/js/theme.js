56ead475b76b895ecb7159401c77b9b6952cb5f1
Dmytro Stupak <stupakbf@gmail.com>
2022-08-31 00:58:57 +0300

    std/add & fixes
    add theme iqons
    window up in pag
    card animation

--- src/js/theme.js
+++ src/js/theme.js
@@ -1,42 +1,31 @@
 const checkbox = document.getElementById('checkbox');
+const checkBallIqon = document.querySelector('.ball');
 
 checkbox.addEventListener('click', () => {
-  checkbox.checked?document.body.classList.add("light"):document.body.classList.remove("light");
+  checkbox.checked
+    ? document.body.classList.add('light')
+    : document.body.classList.remove('light');
   localStorage.setItem('lightModeStatus', checkbox.checked);
+
+  // checkBallIqon.iconTheme ?
+  document
+    .querySelector('.theme__iqon-sun')
+    .classList.toggle('theme-iqon-hidden');
+  document
+    .querySelector('.theme__iqon-moon')
+    .classList.toggle('theme-iqon-hidden');
+  // localStorage.setItem('lightModeIqonStatus', checkBallIqon.iconTheme);
 });
 
-window.addEventListener('load', (event) => {
-  if(localStorage.getItem('lightModeStatus')=="true"){
-    document.body.classList.add("light"); 
+window.addEventListener('load', event => {
+  if (localStorage.getItem('lightModeStatus') == 'true') {
+    document.body.classList.add('light');
     document.getElementById('checkbox').checked = true;
   }
+  // if (localStorage.getItem('lightModeIqonStatus') === 'true') {
+  //   document
+  //     .querySelector('.theme__iqon-sun')
+  //     .classList.toggle('theme-iqon-hidden');
+  //   document.querySelector('.ball').checkBallIqon = true;
+  // }
 });
-
-// const toggleBtn = document.getElementById("checkbox");
-// // const theme = document.getElementById("theme"); document.body
-// let darkMode = localStorage.getItem("dark-mode");
-
-// const enableDarkMode = () => {
-//   document.body.classList.add("dark-mode-theme");
-//   toggleBtn.classList.remove("dark-mode-toggle");
-//   localStorage.setItem("dark-mode", "enabled");
-// };
-
-// const disableDarkMode = () => {
-//   document.body.remove("dark-mode-theme");
-//   toggleBtn.classList.add("dark-mode-toggle");
-//   localStorage.setItem("dark-mode", "disabled");
-// };
-
-// if (darkMode === "enabled") {
-//   enableDarkMode(); // set state of darkMode on page load
-// }
-
-// toggleBtn.addEventListener("click", (e) => {
-//   darkMode = localStorage.getItem("dark-mode"); // update darkMode when clicked
-//   if (darkMode === "disabled") {
-//     enableDarkMode();
-//   } else {
-//     disableDarkMode();
-//   }
-// });
