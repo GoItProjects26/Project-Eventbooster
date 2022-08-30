9b76a61f766a2c6fc06d09fb37962fdbcf85f966
skazzp <skazzp@gmail.com>
2022-08-30 12:11:54 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './js/theme';
 
 import './js/onEvent';
 // import './js/pagination/pagination1';
-import './js/pagination';
+// import './js/pagination';
 import './js/modal.js';
 import './js/getbyid';
 
@@ -17,4 +17,4 @@ import './js/geo/geoApi';
 
 import './js/country/dropDown';
 
-// import './js/pagination/pag';
+import './js/pagination/pag';
