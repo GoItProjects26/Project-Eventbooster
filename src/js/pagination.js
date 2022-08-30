1ba85acddc7f6c12e6a91db95339f18ad119b9a9
Dmytro Stupak <stupakbf@gmail.com>
2022-08-30 15:14:09 +0300

    std/theme switcher position

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -92,9 +92,11 @@ function onPageClick(event) {
     EventApi.setPage(+event.target.dataset.page);
     // EventApi.setKeyword('NBA');
     // renderMarckupFromLocalStorage();
+
     // setTimeout(() => {
     //   window.scrollTo(0, 0);
     // }, 1000);
+
     renderMarckup();
     pag1();
   }
