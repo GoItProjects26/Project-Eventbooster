e46da132a981fd3ac922c3f66dfeb8821efeea51
Maksymenko Maksym <maksymenko1992@ukr.net>
2022-08-30 10:56:55 +0300

    1

--- src/js/load/load.js
+++ src/js/load/load.js
@@ -1,6 +1,7 @@
 import { EventApi } from "../api";
 
 export async function saveCollectionToLocalStorage() {
+    console.log('123456');
     if (localStorage.getItem("event")) {
         return
     }
