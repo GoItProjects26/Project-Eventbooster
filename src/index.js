8c6c013c25ff4a5c20c60b665b9aa1338326e226
skazzp <skazzp@gmail.com>
2022-08-30 11:37:47 +0300

    test

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './js/theme';
 
 import './js/onEvent';
 // import './js/pagination/pagination1';
-// import './js/pagination';
+import './js/pagination';
 import './js/modal.js';
 import './js/getbyid';
 
@@ -13,18 +13,8 @@ import './js/sticky';
 import './js/inputs/search-form';
 import './js/inputs/country-form';
 
-
-
-
-
-
-
-
-
-
 import './js/geo/geoApi';
 
 import './js/country/dropDown';
 
-import './js/pagination/pag';
-
+// import './js/pagination/pag';
