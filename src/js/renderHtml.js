5d7b1ad646adb13412ca86fbe809a9236c513dd4
GoItProjects26 <112173159+GoItProjects26@users.noreply.github.com>
2022-09-01 00:11:18 +0300

    Update renderHtml.js

--- src/js/renderHtml.js
+++ src/js/renderHtml.js
@@ -97,7 +97,7 @@ function templateItems(event) {
                       type="image/jpg"
                     />
 
-                     <img
+                     <img class="event__pic"
             src="${event.mobImg1x}"
             alt="${event.name}"
             width="120"
