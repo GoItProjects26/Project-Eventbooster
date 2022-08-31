5be62fe27a94e8719ae9787b8b56325813ee332f
seacat271 <seacat271083@gmail.com>
2022-08-31 20:56:43 +0300

    button close

--- src/js/basket.js
+++ src/js/basket.js
@@ -233,3 +233,20 @@ export function clearAfterSignOut () {
     onBasketEmpty();
     deleteTimer(timerId)
 }
+
+
+refs.basketMarkupContainer.addEventListener("click", onClickDeleteButton);
+function onClickDeleteButton(event) {
+  if(event.target.closest(".close__box") === null) return;
+
+  event.target.closest("LI").dataset.id
+ 
+  for (let i = 0; i <= userBasket.contentShoppingCart.length - 1; i +=1) {
+    if (userBasket.contentShoppingCart[i].id === event.target.closest("LI").dataset.id){
+        userBasket.contentShoppingCart.splice(i, 1)
+        userBasket.decreaseStandardQuantity()
+        break;
+    }
+  }
+  updateBasket()
+}
